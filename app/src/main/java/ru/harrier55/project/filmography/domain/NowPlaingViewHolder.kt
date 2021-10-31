865cfe570c000343f0a58159379dd38b04b34357
Harrier55 <harrier55@yandex.ru>
2021-10-31 21:05:42 +0500

    add card_films_layout

---
+++ app/src/main/java/ru/harrier55/project/filmography/domain/NowPlaingViewHolder.kt
@@ -0,0 +1,9 @@
+package ru.harrier55.project.filmography.domain
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NowPlayingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+}
---
+++ app/src/main/java/ru/harrier55/project/filmography/domain/NowPlayingAdapter.kt
@@ -0,0 +1,19 @@
+package ru.harrier55.project.filmography.domain
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NowPlayingAdapter: RecyclerView.Adapter<NowPlayingViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NowPlayingViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NowPlayingViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
