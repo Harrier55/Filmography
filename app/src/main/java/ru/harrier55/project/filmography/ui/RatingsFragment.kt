f93bea867a2978a372132613935b207c63f4f41d
Harrier55 <harrier55@yandex.ru>
2021-11-29 22:06:48 +0500

    received a response from the server

--- app/src/main/java/ru/harrier55/project/filmography/ui/RatingsFragment.kt
+++ app/src/main/java/ru/harrier55/project/filmography/ui/RatingsFragment.kt
@@ -1,26 +1,34 @@
 package ru.harrier55.project.filmography.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import okhttp3.*
 import ru.harrier55.project.filmography.R
 import ru.harrier55.project.filmography.databinding.FragmentListFilmBinding
 import ru.harrier55.project.filmography.databinding.FragmentRatingsBinding
+import java.io.IOException
+
 import java.net.URL
 import java.sql.Connection
 
 
 class RatingsFragment : Fragment() {
 
+    private val TAG: String = "@@@"
+
 
     // Готовый пример запроса для получения информации о фильме "Джентельмены
-    private val TESTURL:String = " https://cloud-api.kinopoisk.dev/movies/1143242/token/7d59f07c9c5ce970ffd275a2a7962a0f"
+    var TESTURL:String = " https://cloud-api.kinopoisk.dev/movies/1143242/token/7d59f07c9c5ce970ffd275a2a7962a0f"
+
 
     private var _binding: FragmentRatingsBinding? = null
     private val binding get() = _binding!!
 
+    private val okHttpClient = OkHttpClient()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,14 +41,30 @@ class RatingsFragment : Fragment() {
 
         binding.loadButton.setOnClickListener{
 
-            var connection: Connection? = null
-            var url = URL(TESTURL)
+            val request = Request.Builder()
+                .url(TESTURL)
+                .build()
+
+            okHttpClient.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                    Log.d(TAG, "onFailure: ")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.d(TAG, "onResponse: ")
+
+                    activity?.runOnUiThread {
+                        binding.loadResultTV.text = response.toString()
+                    }
+                }
+
+
+            })
         }
 
-
-
         return view
-
-
     }
+
+
 }
